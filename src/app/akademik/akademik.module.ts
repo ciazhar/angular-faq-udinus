992046ddd285472d4220eaea5b597b2c8d218720
ciazhar <daisetsunakama@gmail.com>
2017-05-25 08:28:16 +0700

    routing module akademik

--- src/app/akademik/akademik.module.ts
+++ src/app/akademik/akademik.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { RouterModule, Routes } from '@angular/router';
+
 import { AktifasiComponent } from './aktifasi/aktifasi.component';
 import { HeregristrasiComponent } from './heregristrasi/heregristrasi.component';
 import { IjazahComponent } from './ijazah/ijazah.component';
@@ -11,9 +14,23 @@ import { RegristrasiComponent } from './regristrasi/regristrasi.component';
 import { TranskripComponent } from './transkrip/transkrip.component';
 import { WisudaComponent } from './wisuda/wisuda.component';
 
+const routingAkademik : Routes = [
+  { path: "akademik/aktifasi", component: AktifasiComponent },
+  { path: "akademik/heregristrasi", component: HeregristrasiComponent },
+  { path: "akademik/ijazah", component: IjazahComponent },
+  { path: "akademik/informasi-kelas-kosong", component: InformasiKelasKosongComponent },
+  { path: "akademik/krs", component: KrsComponent },
+  { path: "akademik/ktm", component: KtmComponent },
+  { path: "akademik/permohonan-cuti", component: PermohonanCutiComponent },
+  { path: "akademik/regristrasi", component: RegristrasiComponent },
+  { path: "akademik/transkrip", component: TranskripComponent },
+  { path: "akademik/wisuda", component: WisudaComponent }
+]
+
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, RouterModule.forChild(routingAkademik)
   ],
   declarations: [AktifasiComponent, HeregristrasiComponent, IjazahComponent, InformasiKelasKosongComponent, KrsComponent, KtmComponent, PermohonanCutiComponent, RegristrasiComponent, TranskripComponent, WisudaComponent]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AkademikModule } from './akademik/akademik.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+
+const routingAplikasi: Routes = [
+  { path: "akademik", redirectTo: "/transaksi", pathMatch: "full"},
+]
+
 
 @NgModule({
   declarations: [
@@ -18,7 +26,9 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AkademikModule,
+    RouterModule.forRoot(routingAplikasi)
   ],
   providers: [],
   bootstrap: [AppComponent]
